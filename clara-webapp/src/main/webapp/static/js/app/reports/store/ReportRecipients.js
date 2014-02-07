b7452b78c5d0bdfdc3d255b1573340384dec3809
Matt Baker <minorbug@gmail.com>
2014-02-07 14:11:30 -0600

    REPORTS: Changed email field to multiple select
    Signed-off-by: Jiang Bian <jbian@uams.edu>

---
+++ clara-webapp/src/main/webapp/static/js/app/reports/store/ReportRecipients.js
@@ -0,0 +1,7 @@
+Ext.define('Clara.Reports.store.ReportRecipients', {
+    extend: 'Ext.data.Store',
+    requires: 'Clara.LetterBuilder.model.Recipient',
+    model: 'Clara.LetterBuilder.model.Recipient',
+    autoLoad:false
+
+});
